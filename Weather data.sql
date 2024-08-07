c67628289984f367c35326de412afcb4a6136481
Carolajando <carolajando99@gmail.com>
2024-08-07 23:05:17 +0300

    Add files via upload

---
+++ Weather data.sql
@@ -0,0 +1,24 @@
+SELECT * FROM `weather data`.`weather data`;
+
+
+
+SELECT * 
+ FROM `weather data`.`weather data`
+ WHERE 'Wind Speed_km/h' > 24  
+       AND
+        'Visibility_km' = 25 ;
+ 
+ 
+ SELECT AVG (Visibility_km)
+ FROM `weather data`.`weather data`;
+ 
+ 
+ 
+
+SELECT *
+FROM `weather data`.`weather data`
+
+
+ 
+ 
+ 
